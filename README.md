ec885363d8cdcc1424e89fc7fd11127fdfeb1d12
iliasFs <110726288+iliasFs@users.noreply.github.com>
2023-03-24 00:19:59 +0200

    register user express app ready working

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Airbnb---MERN
+
+Creating an Airbnb clone from scratch
+Using Tailwind for styling
+React MongoDB Express and nodeJs
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,15 +1,48 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const RegisterPage = () => {
+  const [name, setName] = useState("");
+
+  const [email, setEmail] = useState("");
+
+  const [password, setPassword] = useState("");
+
+  //request to API
+  const registerUser = (event) => {
+    event.preventDefault();
+    axios.post("/register", {
+      name,
+      email,
+      password,
+    });
+  };
+
   return (
     <div className="mt-4 grow flex items-center justify-around">
       <div className="mb-64">
         <h1 className="text-xl text-center mb-4">Register</h1>
-        <form className="max-w-md mx-auto">
-          <input type="text" placeholder="Name and Surname" />
-          <input type="email" placeholder="your@email.com" />
-          <input type="password" placeholder="password" />
+        <form className="max-w-md mx-auto" onSubmit={registerUser}>
+          <input
+            type="text"
+            placeholder="Name and Surname"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="your@email.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <button className="primary">Register</button>
           <div className="text-center py-2 text-gray-500">
             Already have an account?{" "}
